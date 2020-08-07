c31b3de13394666993633eb5bd9bddf806227745
Suman <suman7495@gmail.com>
2020-08-07 19:38:52 +0200

    Deleted Net class.

--- rlbotics/common/approximators.py
+++ rlbotics/common/approximators.py
@@ -58,42 +58,6 @@ class MLP:
         print(self.model)
 
 
-class Net(nn.Module):
-    def __init__(self, IO_sizes, hidden_sizes, activations, layer_types):
-        super(Net, self).__init__()
-        self.layers = nn.ModuleList()
-        self.activations = nn.ModuleList()
-
-        layer_sizes = [IO_sizes[0]] + hidden_sizes + [IO_sizes[1]]
-        for i in range(len(layer_sizes) - 1):
-            if layer_types[i] == 'linear':
-                self.layers.append(nn.Linear(layer_sizes[i], layer_sizes[i + 1]))
-            elif layer_types[i] == 'conv':
-                raise NotImplementedError('Conv nets not supported')
-            elif layer_types[i] == 'lstm':
-                raise NotImplementedError('LSTM nets not supported')
-            else:
-                raise NameError(str(layer_types[i]) + ' Layer is not supported')
-
-        for activation in activations:
-            if activation == 'relu':
-                self.activations.append(nn.ReLU())
-            elif activation == 'tanh':
-                self.activations.append(nn.Tanh())
-            elif activation == 'sigmoid':
-                self.activations.append(nn.Sigmoid())
-            elif activation == 'softmax':
-                self.activations.append(nn.LogSoftmax())
-            elif activation == None:
-                self.activations.append(None)
-            else:
-                raise NameError(str(activation) + ' activation not supported')
-
-    def forward(self, x):
-        for i in range(len(self.layers)):
-            x = self.activations[i](self.layers[i](x)) if self.activations[i] else self.layers[i](x)
-        return x
-
 
 class MLPBase(nn.Module):
     def __init__(self, layer_sizes, activations):
