b6cc0aaedf8612e4bb479b78d3041bab7bc139b1
kousheekc <kousheekc@gmail.com>
2020-08-08 12:22:14 +0530

    removed train method from VPG class and minor corrections

--- rlbotics/vpg/main.py
+++ rlbotics/vpg/main.py
@@ -1,20 +1,79 @@
 from rlbotics.vpg.vpg import *
 import gym
+import time
+import matplotlib.pyplot as plt
 
-env = gym.make('CartPole-v1')
+def train(model, env, batch_size, render=False):
+    obs_batch = []
+    act_batch = []
+    rew_batch = []
 
-model = Reinforce(env)
+    episode_rewards = []
 
-render = False
+    new_obs = env.reset()
+    done = False
+    finished_rendering_this_epoch = False
 
-max_reward_per_epoch = []
+    while True:
+        if (not finished_rendering_this_epoch) and render:
+            env.render()
+            time.sleep(0.02)
 
-for epoch in range(100):
-    if epoch % 10 == 0:
-        print("epoch : ", epoch)
-        render = False
+        obs_batch.append(new_obs.copy())
 
-    else:
-        render = False
+        action = model.get_action(new_obs)
+        new_obs, rew, done, info = env.step(action)
 
-    model.train(100, render=False)
+        act_batch.append(action)
+        episode_rewards.append(rew)
+
+        # failed or goal reached
+        if done:
+            finished_rendering_this_epoch = True
+
+            episode_return = sum(episode_rewards)
+            episode_len = len(episode_rewards)
+
+            rew_batch += [episode_return] * episode_len
+
+            new_obs, done, episode_rewards = env.reset(), False, []
+
+            if len(obs_batch) >= batch_size:
+                break
+
+    env.close()
+    model.update_policy(obs_batch, act_batch, rew_batch)
+
+    # for testing
+    return max(rew_batch)
+
+def main():
+    env = gym.make('CartPole-v1')
+
+    model = VPG(env)
+
+    render = False
+
+    max_reward_per_epoch = []
+
+    for epoch in range(1000):
+        if epoch % 10 == 0:
+            print("epoch : ", epoch)
+            render = False
+
+        else:
+            render = False
+
+        max_reward_per_epoch.append(train(model, env, 1000, render=render))
+
+    plt.xlabel('epochs')
+    plt.ylabel('max reward')
+
+    plt.plot(max_reward_per_epoch, label = "max reward in epoch")
+
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
--- rlbotics/vpg/vpg.py
+++ rlbotics/vpg/vpg.py
@@ -2,65 +2,25 @@ from rlbotics.common.policies import *
 import rlbotics.vpg.hyperparameters as h
 import time
 
-class Reinforce:
+class VPG:
     def __init__(self, env):
         self.env = env
         self.obs_dim = self.env.observation_space.shape[0]
         self.act_dim = self.env.action_space.n
-        self.build_policy()
+        self._build_policy()
 
-    def build_policy(self):
+    def _build_policy(self):
         self.policy = MLPSoftmaxPolicy([self.obs_dim, self.act_dim], h.hidden_layers, h.activations, h.layer_types, lr=h.lr)
         self.policy.summary()
 
-    def compute_loss(self, obs, acts, rews):
-        rews_tensor = torch.as_tensor(rews, dtype=torch.float32)
-        logp = self.policy.get_log_prob(obs, acts)
+    def _loss(self, obs_batch, act_batch, rew_batch):
+        rews_tensor = torch.as_tensor(rew_batch, dtype=torch.float32)
+        logp = self.policy.get_log_prob(obs_batch, act_batch)
         return -(logp * rews_tensor).mean()
 
+    def get_action(self, obs):
+        return self.policy.get_action(obs)
 
-    def update_policy(self, batch_obs, batch_acts, batch_rews):
-        loss = self.compute_loss(batch_obs, batch_acts, batch_rews)
+    def update_policy(self, obs_batch, act_batch, rew_batch):
+        loss = self._loss(obs_batch, act_batch, rew_batch)
         self.policy.train(loss)
-
-    def train(self, batch_size, render=False):
-        batch_obs = []
-        batch_acts = []
-        batch_rewards = []
-
-        episode_rewards = []
-
-        obs = self.env.reset()
-        done = False
-        finished_rendering_this_epoch = False
-
-        while True:
-            if (not finished_rendering_this_epoch) and render:
-                self.env.render()
-                time.sleep(0.02)
-
-            batch_obs.append(obs.copy())
-
-            action = self.policy.get_action(obs)
-            obs, reward, done, info = self.env.step(action)
-
-            batch_acts.append(action)
-            episode_rewards.append(reward)
-
-            # failed or goal reached
-            if done:
-                finished_rendering_this_epoch = True
-
-                episode_return = sum(episode_rewards)
-                episode_len = len(episode_rewards)
-
-                batch_rewards += [episode_return] * episode_len
-
-                obs, done, episode_rewards = self.env.reset(), False, []
-
-                if len(batch_obs) >= batch_size:
-                    break
-
-        self.env.close()
-
-        self.update_policy(batch_obs, batch_acts, batch_rewards)
