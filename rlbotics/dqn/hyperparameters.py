691d8b961c727623a981a7f664191c22af7c9504
Dyuman Aditya <dyuman2001@gmail.com>
2020-08-09 09:48:18 +0530

    updated hyperparameters.py

--- rlbotics/dqn/hyperparameters.py
+++ rlbotics/dqn/hyperparameters.py
@@ -1,20 +1,20 @@
 # General Parameters:
 env_name      = 'CartPole-v1'
 gamma         = 0.95
-lr            = 0.01
+lr            = 0.001
 num_episodes  = 1000
 render        = False
 
 # DQN Specific:
 epsilon 	  = 1.0
-min_epsilon   = 0.05
+min_epsilon   = 0.01
 epsilon_decay = 0.995
 batch_size    = 128
-buffer_size  = 2000
+buffer_size   = 6000
 
 # Policy Network:
 start_learning = 500            # Start learning after some experience
-hidden_sizes   = [64, 64] 	# Dimensions have to be 1 less than activations
+hidden_sizes   = [128, 128] 	# Dimensions have to be 1 less than activations
 activations    = ['relu', 'relu', 'none']
 optimizer      = 'Adam'
 loss           = 'mse'
