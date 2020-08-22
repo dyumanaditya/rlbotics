fixed 0 for gradient of surrogate loss

rlbotics/ppo/ppo.py
@@ -17,7 +17,6 @@ class PPO:
         self.gamma = args.gamma
         self.lam = args.lam
         self.seed = args.seed
-        self.batch_size = args.batch_size
         self.num_value_iters = args.num_value_iters
         self.num_policy_iters = args.num_policy_iters
 
@@ -86,6 +85,9 @@ class PPO:
         rew_batch = torch.as_tensor(transition_batch.rew, dtype=torch.float32)
         done_batch = torch.as_tensor(transition_batch.done, dtype=torch.int32)
 
+        old_log_prob = self.policy.get_log_prob(obs_batch, act_batch)
+        old_policy = self.policy.get_distribution(obs_batch)
+
         expected_return = get_expected_return(rew_batch, done_batch, self.gamma)
         values = torch.flatten(self.value.predict(obs_batch))
 
@@ -97,7 +99,9 @@ class PPO:
                     act=act_batch,
                     val=values,
                     adv=adv_batch,
-                    ret=expected_return)
+                    ret=expected_return,
+                    old_log_prob=old_log_prob,
+                    old_policy=old_policy)
         return data
 
     def compute_policy_loss(self):
@@ -129,9 +133,6 @@ class PPO:
 
     def update_policy(self):
         self.data = self._get_data()
-        obs, act = self.data["obs"], self.data["act"]
-        self.data["old_log_prob"] = self.policy.get_log_prob(obs, act)
-        self.data["old_policy"] = self.policy.get_distribution(obs)
 
         for _ in range(self.num_policy_iters):
             self.policy.optimizer.zero_grad()

rlbotics/trpo/trpo.py
@@ -18,7 +18,6 @@ class TRPO:
         self.gamma = args.gamma
         self.lam = args.lam
         self.seed = args.seed
-        self.batch_size = args.batch_size
         self.num_v_iters = args.num_v_iters
 
         # TRPO specific hyperparameters
@@ -85,6 +84,9 @@ class TRPO:
         rew_batch = torch.as_tensor(transition_batch.rew, dtype=torch.float32)
         done_batch = torch.as_tensor(transition_batch.done, dtype=torch.int32)
 
+        old_log_prob = self.policy.get_log_prob(obs_batch, act_batch)
+        old_policy = self.policy.get_distribution(obs_batch)
+
         expected_return = get_expected_return(rew_batch, done_batch, self.gamma)
         values = torch.flatten(self.value.predict(obs_batch))
 
@@ -96,7 +98,9 @@ class TRPO:
                     act=act_batch,
                     val=values,
                     adv=adv_batch,
-                    ret=expected_return)
+                    ret=expected_return,
+                    old_log_prob=old_log_prob,
+                    old_policy=old_policy)
         return data
 
     def compute_policy_loss(self):
@@ -111,7 +115,7 @@ class TRPO:
         new_log_prob = self.policy.get_log_prob(obs, act)
         old_log_prob = self.data["old_log_prob"]
 
-        L = torch.mul(torch.exp(new_log_prob - old_log_prob), adv).mean()
+        L = torch.mul(torch.exp(new_log_prob - old_log_prob.detach()), adv).mean()
         kl = torch.distributions.kl.kl_divergence(old_policy, new_policy).mean()
         ent = new_policy.entropy().mean()
 
@@ -122,9 +126,6 @@ class TRPO:
 
     def update_policy(self):
         self.data = self._get_data()
-        obs, act, adv = self.data["obs"],  self.data["act"],  self.data["adv"]
-        self.data["old_log_prob"] = self.policy.get_log_prob(obs, act)
-        self.data["old_policy"] = self.policy.get_distribution(obs)
 
         L, kl, ent = self.compute_policy_loss()
 

rlbotics/trpo/utils.py
@@ -1,5 +1,6 @@
 import torch
 
 def flat_grad(y, x):
-    grads = torch.autograd.grad(y, x)
-    return torch.cat([torch.reshape(g, [-1]) for g in grads], axis=0)
+    g = torch.autograd.grad(y, x)
+    g = torch.cat([t.view(-1) for t in g])
+    return g

rlbotics/vpg/vpg.py
@@ -17,7 +17,6 @@ class VPG:
         self.gamma = args.gamma
         self.lam = args.lam
         self.seed = args.seed
-        self.batch_size = args.batch_size
         self.num_v_iters = args.num_v_iters
 
         # Policy Network
@@ -81,6 +80,9 @@ class VPG:
         rew_batch = torch.as_tensor(transition_batch.rew, dtype=torch.float32)
         done_batch = torch.as_tensor(transition_batch.done, dtype=torch.int32)
 
+        old_log_prob = self.policy.get_log_prob(obs_batch, act_batch)
+        old_policy = self.policy.get_distribution(obs_batch)
+
         expected_return = get_expected_return(rew_batch, done_batch, self.gamma)
         values = torch.flatten(self.value.predict(obs_batch))
 
@@ -92,7 +94,9 @@ class VPG:
                     act=act_batch,
                     val=values,
                     adv=adv_batch,
-                    ret=expected_return)
+                    ret=expected_return,
+                    old_log_prob=old_log_prob,
+                    old_policy=old_policy)
         return data
 
     def compute_policy_loss(self, obs_batch, act_batch, adv_batch):