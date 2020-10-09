3782252fafb8433a618cc0f155ced45c7b6ef1c1
DyumanAditya <dyuman2001@gmail.com>
2020-10-09 12:21:18 +0530

    driller env with cam and obj + urdf files locally

--- rlbotics/envs/panda_driller/panda_driller.py
+++ rlbotics/envs/panda_driller/panda_driller.py
@@ -1,6 +1,7 @@
+import os
 import gym
-import math
 import time
+import numpy as np
 import pybullet as p
 import pybullet_data
 from gym.utils import seeding
@@ -11,6 +12,7 @@ class PandaDrillerEnv(gym.Env):
 				'video.frames_per_second': 60}
 
 	def __init__(self, render):
+		self.path = os.path.abspath(os.path.dirname(__file__))
 		self.isRender = render
 		self.seed()
 		self.reset()
@@ -27,40 +29,99 @@ class PandaDrillerEnv(gym.Env):
 			p.connect(p.DIRECT)
 
 		p.resetSimulation()
-		# p.setGravity(0, 0, -9.8)
+		# p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
 
 		# Load Robots
 		p.setAdditionalSearchPath(pybullet_data.getDataPath())
-		tableOrientation = p.getQuaternionFromEuler([0, 0, math.pi/2])
-		drillOrientation = p.getQuaternionFromEuler([0, -math.pi/2, math.pi])
+		tableOrientation = p.getQuaternionFromEuler([0, 0, np.pi/2])
+		drillOrientation = p.getQuaternionFromEuler([0, -np.pi/2, np.pi])
 
-		self.planeId = p.loadURDF('plane.urdf')
-		self.armId = p.loadURDF('franka_panda/panda.urdf', [0, 0, 0.93], useFixedBase=True)
-		self.drillId = p.loadURDF('drill/drill.urdf', [0.335, 0, 1.73], drillOrientation, globalScaling=0.013)
-		self.tableId = p.loadURDF('table/table.urdf', [0.5, 0, 0], tableOrientation, globalScaling=1.5, useFixedBase=True)
+		self.plane_id = p.loadURDF('plane.urdf')
+		self.arm_id = p.loadURDF('franka_panda/panda.urdf', [-0.6, 0, 0.93], useFixedBase=True)
+		self.drill_id = p.loadURDF(os.path.join(self.path, 'drill.urdf'), [-0.265, 0, 1.73], drillOrientation, globalScaling=0.013)
+		self.table_id = p.loadURDF('table/table.urdf', [0, 0, 0], tableOrientation, globalScaling=1.5, useFixedBase=True)
 
-		time.sleep(5)
 		p.setRealTimeSimulation(1)
 		self._grab_drill()
-		p.setRealTimeSimulation(0)
+		self._generate_plane()
+		self._get_camera_img()
+		# p.setRealTimeSimulation(0)
 
 	def _grab_drill(self):
 		time.sleep(0.5)
-		p.setJointMotorControl2(self.armId, 5, p.POSITION_CONTROL, targetPosition=1)
-		p.setJointMotorControl2(self.armId, 3, p.POSITION_CONTROL, targetPosition=-1)
-		p.setJointMotorControl2(self.armId, 6, p.POSITION_CONTROL, targetPosition=0.8)
-		p.setJointMotorControl2(self.armId, 9, p.POSITION_CONTROL, targetPosition=0.5)
-		p.setJointMotorControl2(self.armId, 10, p.POSITION_CONTROL, targetPosition=0.5)
+		p.setJointMotorControl2(self.arm_id, 5, p.POSITION_CONTROL, targetPosition=1)
+		p.setJointMotorControl2(self.arm_id, 3, p.POSITION_CONTROL, targetPosition=-1)
+		p.setJointMotorControl2(self.arm_id, 6, p.POSITION_CONTROL, targetPosition=0.8)
+		p.setJointMotorControl2(self.arm_id, 9, p.POSITION_CONTROL, targetPosition=0.5)
+		p.setJointMotorControl2(self.arm_id, 10, p.POSITION_CONTROL, targetPosition=0.5)
 
 		time.sleep(0.5)
-		p.setJointMotorControl2(self.armId, 9, p.POSITION_CONTROL, targetPosition=-0.5)
-		p.setJointMotorControl2(self.armId, 10, p.POSITION_CONTROL, targetPosition=-0.5)
+		p.setJointMotorControl2(self.arm_id, 9, p.POSITION_CONTROL, targetPosition=-0.5)
+		p.setJointMotorControl2(self.arm_id, 10, p.POSITION_CONTROL, targetPosition=-0.5)
 
 		time.sleep(0.5)
 		p.setGravity(0, 0, -9.8)
-		p.setJointMotorControl2(self.armId, 3, p.POSITION_CONTROL, targetPosition=1)
+		p.setJointMotorControl2(self.arm_id, 3, p.POSITION_CONTROL, targetPosition=1)
 		time.sleep(0.5)
 
+	def _generate_plane(self):
+		# Min Max constraints for drilling on plane
+		# min = [-0.2, 0.2, 0]
+		# max = [0.2, -0.2, 0]
+
+		hole_position = [self.np_random.uniform(-0.2, 0.2), self.np_random.uniform(-0.2, 0.2), 0]
+		plane_orientation = p.getQuaternionFromEuler(list(self.np_random.uniform(low=0, high=2*np.pi, size=3)))
+		# plane_orientation = p.getQuaternionFromEuler([0, 0, np.pi/2])
+
+		plane_visual = p.createVisualShape(
+			p.GEOM_MESH,
+			fileName=os.path.join(self.path, 'plane', 'plane.obj')
+		)
+
+		plane = p.createMultiBody(
+			basePosition=[0, 0, 1.3],
+			baseVisualShapeIndex=plane_visual,
+			baseOrientation=plane_orientation,
+			baseInertialFramePosition=[0.6, 0, 1.3]
+		)
+
+		# Wood texture for plane
+		texture = p.loadTexture(os.path.join(self.path, 'plane', 'texture.jpeg'))
+		p.changeVisualShape(plane, -1, textureUniqueId=texture)
+
+		hole_visual = p.createVisualShape(
+			p.GEOM_MESH,
+			rgbaColor=[255, 0, 0, 1],
+			visualFramePosition=hole_position,
+			fileName=os.path.join(self.path, 'plane', 'targetHole0_01.obj')
+		)
+
+		hole = p.createMultiBody(
+			basePosition=[0, 0, 1.3],
+			baseVisualShapeIndex=hole_visual,
+			baseOrientation=plane_orientation
+		)
+
+	def _get_camera_img(self):
+		view_matrix = p.computeViewMatrix(
+			cameraEyePosition=[0, 0, 3],
+			cameraTargetPosition=[0, 0, 1.3],
+			cameraUpVector=[0, 1, 0]
+		)
+
+		projection_matrix = p.computeProjectionMatrixFOV(
+			fov=45,
+			aspect=1.0,
+			nearVal=0.2,
+			farVal=1.5
+		)
+
+		width, height, rgb_img, depth_img, seg_img = p.getCameraImage(
+			width=1000,
+			height=1000,
+			viewMatrix=view_matrix,
+			projectionMatrix=projection_matrix
+		)
 
 	def render(self, mode='human'):
 		if mode == 'human':
@@ -89,6 +150,7 @@ class PandaDrillerEnv(gym.Env):
 env = PandaDrillerEnv(render=True)
 while 1:
 	time.sleep(0.1)
+	env._get_camera_img()
 
 # act = [12, 20] * 6
 # for i in range(100):
