e7c66857562cf70203c95a14dbbbe28b0d58b414
kousheekc <kousheekc@gmail.com>
2020-10-10 23:58:59 +0530

    reward

--- rlbotics/envs/panda_gripper/panda_gripper.py
+++ rlbotics/envs/panda_gripper/panda_gripper.py
@@ -9,10 +9,10 @@ class PandaGripperEnv(gym.Env):
 	metadata = {'render.modes': ['human', 'rgb_array'],
 				'video.frames_per_second': 60}
 
-	def __init__(self, render, firstPersonView=False, numOfCubes=10):
-		self.isRender = render
-		self.numOfCubes = numOfCubes
-		self.firstPersonView = firstPersonView
+	def __init__(self, render, first_person_view=False, num_of_cubes=10):
+		self.is_render = render
+		self.num_of_cubes = num_of_cubes
+		self.first_person_view = first_person_view
 		self.seed()
 		self.reset()
 
@@ -22,7 +22,7 @@ class PandaGripperEnv(gym.Env):
 
 	def reset(self):
 		# Connect to physics client
-		if self.isRender:
+		if self.is_render:
 			p.connect(p.GUI)
 		else:
 			p.connect(p.DIRECT)
@@ -31,30 +31,34 @@ class PandaGripperEnv(gym.Env):
 		p.setGravity(0, 0, -9.8)
 		p.setRealTimeSimulation(True)
 
-		# Load Robots
 		p.setAdditionalSearchPath(pybullet_data.getDataPath())
-		tableOrientation = p.getQuaternionFromEuler([0, 0, np.pi/2])
+		table_orientation = p.getQuaternionFromEuler([0, 0, np.pi/2])
 
 
-		self.planeId = p.loadURDF('plane.urdf')
-		self.armId = p.loadURDF('franka_panda/panda.urdf', [0, 0, 0.94], useFixedBase=True)
-		self.tableId = p.loadURDF('table/table.urdf', [0.5, 0, 0], tableOrientation, globalScaling=1.5, useFixedBase=True)
+		self.plane_id = p.loadURDF('plane.urdf')
+		self.arm_id = p.loadURDF('franka_panda/panda.urdf', [0, 0, 0.94], useFixedBase=True)
+		self.table_id = p.loadURDF('table/table.urdf', [0.5, 0, 0], table_orientation, globalScaling=1.5, useFixedBase=True)
 
-		self.fromTrayId = p.loadURDF('tray/traybox.urdf', [0.5, 0.4, 0.94])
-		self.toTrayId = p.loadURDF('tray/traybox.urdf', [0.5, -0.4, 0.94])
+		self.from_tray_id = p.loadURDF('tray/traybox.urdf', [0.5, 0.4, 0.94])
+		self.to_tray_id = p.loadURDF('tray/traybox.urdf', [0.5, -0.4, 0.94])
 
-		self.cubesId = []
+		self.cubes_id = []
+		self.other_id = [self.table_id, self.arm_id, self.from_tray_id, self.to_tray_id]
 
-		for i in range(self.numOfCubes):
+		self.num_of_joints = p.getNumJoints(self.arm_id)
+		self.limits = self.get_limits()
+		p.setJointMotorControlArray(self.arm_id, list(range(self.num_of_joints)), controlMode=p.POSITION_CONTROL, targetPositions=[0]*self.num_of_joints)
+
+		time.sleep(20)
+
+		for i in range(self.num_of_cubes):
 			cubeOrientation = p.getQuaternionFromEuler([np.random.uniform(0, 2*np.pi), np.random.uniform(0, 2*np.pi), np.random.uniform(0, 2*np.pi)])
-			self.cubesId.append(p.loadURDF('cube_small.urdf', [np.random.uniform(0.3, 0.7), np.random.uniform(0.2, 0.6), 2], cubeOrientation))
+			self.cubes_id.append(p.loadURDF('cube_small.urdf', [np.random.uniform(0.3, 0.7), np.random.uniform(0.2, 0.6), 2], cubeOrientation))
 
 		time.sleep(1)
 
-		self.numOfJoints = p.getNumJoints(self.armId)
-
-		for i in range(self.numOfJoints):
-			print(p.getJointInfo(self.armId, i))
+		# for i in range(self.num_of_joints):
+		# 	print(p.getJointInfo(self.arm_id, i))
 
 		self.init_up_vector = (1, 0, 0)
 		self.init_camera_vector = (0, 0, 1)
@@ -62,7 +66,7 @@ class PandaGripperEnv(gym.Env):
 		fov, aspect, nearplane, farplane = 60, 1.0, 0.01, 100
 		self.projection_matrix = p.computeProjectionMatrixFOV(fov, aspect, nearplane, farplane)
 
-		if self.firstPersonView:
+		if self.first_person_view:
 			pass
 
 		else:
@@ -73,9 +77,18 @@ class PandaGripperEnv(gym.Env):
 
 		return img
 
+	def get_limits(self):
+		limits = []
+
+		for i in range(self.num_of_joints):
+			#print(p.getJointInfo(arm_id, i)[8:10])
+			limits.append(p.getJointInfo(self.arm_id, i)[8:10])
+
+		return limits
+
 	def render(self, mode='human'):
 		if mode == 'human':
-			self.isRender = True
+			self.is_render = True
 		elif mode == 'rgb_array':
 			pass
 
@@ -83,25 +96,70 @@ class PandaGripperEnv(gym.Env):
 		reward = 0
 		done = 0
 
-		for joint in range(self.numOfJoints):
-			p.setJointMotorControl2(self.armId, joint, controlMode=p.POSITION_CONTROL, targetPosition=action[joint])
+		action = np.clip(action, -1, 1).astype(np.float32)
 
-		if self.firstPersonView:
+		for i in range(self.num_of_joints):
+			action[i] = self._linear_map(action[i], -1, 1, self.limits[i][0], self.limits[i][1])
+
+		# bounding box of from tray and to tray
+		from_tray_min_AABB, from_tray_max_AABB = p.getAABB(self.from_tray_id)
+		to_tray_min_AABB, to_tray_max_AABB = p.getAABB(self.to_tray_id)
+
+		# ids of overlapping objects with from tray and to tray
+		overlapping_objects_with_from_tray = np.array(p.getOverlappingObjects(from_tray_min_AABB, from_tray_max_AABB))[:, 0]
+		overlapping_objects_with_to_tray = np.array(p.getOverlappingObjects(to_tray_min_AABB, to_tray_max_AABB))[:, 0]
+
+		# ids of overlapping cubes with from tray and to tray
+		overlapping_cubes_with_from_tray = [id for id in overlapping_objects_with_from_tray if id not in self.other_id]
+		overlapping_cubes_with_to_tray = [id for id in overlapping_objects_with_to_tray if id not in self.other_id]
+
+		# print("num of cubes in from tray is ", len(overlapping_cubes_with_from_tray))
+		# print("num of cubes in to tray is ", len(overlapping_cubes_with_to_tray))
+
+		# reward = number of cubes outside from tray + number of cubes in to tray
+		reward = self.num_of_cubes - len(overlapping_cubes_with_from_tray) + len(overlapping_cubes_with_to_tray)
+
+		if reward == 20:
+			done = 1
+
+		p.setJointMotorControlArray(self.arm_id, list(range(self.num_of_joints)), controlMode=p.POSITION_CONTROL, targetPositions=action)
+		# time.sleep(1)
+
+		if self.first_person_view:
 			pass
-
 		else:
-			img = p.getCameraImage(1000, 1000, self.view_matrix, self.projection_matrix)
-
+			img = p.getCameraImage(100, 100, self.view_matrix, self.projection_matrix)
 
 		return img, reward, done
 
+	def _linear_map(self, x, in_min, in_max, out_min, out_max):
+		return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
 
 env = PandaGripperEnv(render=True)
+
 # while 1:
 # 	time.sleep(0.1)
 
+# for i in range(12):
+# 	positionMin = [0] * 12
+# 	positionMin[i] = env.limits[i][0]
+# 	img, rew, done = env.step(positionMin)
+# 	time.sleep(1)
+# 	print(rew, done)
+#
+# 	positionMax = [0] * 12
+# 	positionMax[i] = env.limits[i][1]
+# 	img, rew, done = env.step(positionMax)
+# 	time.sleep(1)
+# 	print(rew, done)
+#
+# 	positionZero = [0] * 12
+# 	img, rew, done = env.step(positionZero)
+# 	time.sleep(1)
+# 	print(rew, done)
 
-# for i in range(100):
-# 	act = np.random.rand(1, 12).squeeze(0)
-# 	time.sleep(0.1)
-# 	env.step(act)
+for i in range(100):
+	act = np.random.uniform(-1, 1, 12)
+	img, rew, done = env.step(act)
+	print(rew, done)
