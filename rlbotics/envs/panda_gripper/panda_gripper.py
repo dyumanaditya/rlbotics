27d07bc59d5171c3386066c2211af81ac7e7e606
kousheekc <kousheekc@gmail.com>
2020-10-15 17:49:23 +0530

    smooth motion

--- rlbotics/envs/panda_gripper/panda_gripper.py
+++ rlbotics/envs/panda_gripper/panda_gripper.py
@@ -4,6 +4,7 @@ import pybullet as p
 import pybullet_data
 from gym.utils import seeding
 import numpy as np
+import math
 
 class PandaGripperEnv(gym.Env):
 	metadata = {'render.modes': ['human', 'rgb_array'],
@@ -14,14 +15,7 @@ class PandaGripperEnv(gym.Env):
 		self.num_of_cubes = num_of_cubes
 		self.first_person_view = first_person_view
 		self.movement_penalty_constant = 10
-		self.seed()
-		self.reset()
 
-	def seed(self, seed=None):
-		self.np_random, seed = seeding.np_random(seed)
-		return [seed]
-
-	def reset(self):
 		# Connect to physics client
 		if self.is_render:
 			p.connect(p.GUI)
@@ -33,15 +27,18 @@ class PandaGripperEnv(gym.Env):
 		p.setRealTimeSimulation(True)
 
 		p.setAdditionalSearchPath(pybullet_data.getDataPath())
-		table_orientation = p.getQuaternionFromEuler([0, 0, np.pi/2])
 
+		table_orientation = p.getQuaternionFromEuler([0, 0, np.pi/2])
 
 		self.plane_id = p.loadURDF('plane.urdf')
 		self.arm_id = p.loadURDF('franka_panda/panda.urdf', [0, 0, 0.94], useFixedBase=True)
 		self.table_id = p.loadURDF('table/table.urdf', [0.5, 0, 0], table_orientation, globalScaling=1.5, useFixedBase=True)
 
-		self.from_tray_id = p.loadURDF('tray/traybox.urdf', [0.5, 0.4, 0.94])
-		self.to_tray_id = p.loadURDF('tray/traybox.urdf', [0.5, -0.4, 0.94])
+		self.from_tray_pos = [0.5, 0.4, 0.94]
+		self.to_tray_pos = [0.5, -0.4, 0.94]
+
+		self.from_tray_id = p.loadURDF('tray/traybox.urdf', self.from_tray_pos)
+		self.to_tray_id = p.loadURDF('tray/traybox.urdf', self.to_tray_pos)
 
 		# bounding box of from tray and to tray
 		self.from_tray_min_AABB, self.from_tray_max_AABB = p.getAABB(self.from_tray_id)
@@ -50,46 +47,62 @@ class PandaGripperEnv(gym.Env):
 		self.cubes_id = []
 		self.other_id = [self.table_id, self.arm_id, self.from_tray_id, self.to_tray_id]
 
-		self.num_of_joints = p.getNumJoints(self.arm_id)
-		self.limits = self.get_limits()
-		p.setJointMotorControlArray(self.arm_id, list(range(self.num_of_joints)), controlMode=p.POSITION_CONTROL, targetPositions=[0]*self.num_of_joints)
+		# Initialise joint info
+		self.joint_states, self.joint_limits, self.velocity_limits = [], [], []
+		self.num_arm_joints = p.getNumJoints(self.arm_id)
+		for j in range(self.num_arm_joints):
+			self.joint_states.append(p.getJointState(self.arm_id, j)[0])
+			self.joint_limits.append(p.getJointInfo(self.arm_id, j)[8:10])
+			v = p.getJointInfo(self.arm_id, j)[11]
+			self.velocity_limits.append(np.inf if v == 0 else v)
+
+		self.seed()
+		self.reset()
+
+	def seed(self, seed=None):
+		self.np_random, seed = seeding.np_random(seed)
+		return [seed]
+
+	def reset(self):
+		p.setJointMotorControlArray(self.arm_id, list(range(self.num_arm_joints)), controlMode=p.POSITION_CONTROL, targetPositions=[0]*self.num_arm_joints)
 
 		# time.sleep(20)
 
 		for i in range(self.num_of_cubes):
 			cubeOrientation = p.getQuaternionFromEuler([np.random.uniform(0, 2*np.pi), np.random.uniform(0, 2*np.pi), np.random.uniform(0, 2*np.pi)])
-			self.cubes_id.append(p.loadURDF('cube_small.urdf', [np.random.uniform(0.3, 0.7), np.random.uniform(0.2, 0.6), 2], cubeOrientation))
+			self.cubes_id.append(p.loadURDF('cube_small.urdf', [np.random.uniform(self.from_tray_pos[0] - 0.2, self.from_tray_pos[0] + 0.2), np.random.uniform(self.from_tray_pos[1] - 0.2, self.from_tray_pos[1] + 0.2), 2], cubeOrientation))
 
 		time.sleep(1)
 
-		# for i in range(self.num_of_joints):
+		# for i in range(self.num_arm_joints):
 		# 	print(p.getJointInfo(self.arm_id, i))
 
-		self.init_up_vector = (1, 0, 0)
-		self.init_camera_vector = (0, 0, 1)
-
-		fov, aspect, nearplane, farplane = 60, 1.0, 0.01, 100
-		self.projection_matrix = p.computeProjectionMatrixFOV(fov, aspect, nearplane, farplane)
-
-		if self.first_person_view:
-			pass
-
-		else:
-			self.view_matrix = p.computeViewMatrix((0.5,0,2.5),(0.5,0,0.94), self.init_up_vector)
-			img = p.getCameraImage(1000, 1000, self.view_matrix, self.projection_matrix)
-
-		# p.getCameraImage(640, 480)
+		img = self._get_camera_img()
 
 		return img
 
-	def get_limits(self):
-		limits = []
+	def _get_camera_img(self):
+		view_matrix = p.computeViewMatrix(
+			cameraEyePosition=[0.5, 0, 2.5],
+			cameraTargetPosition=[0.5, 0, 0.94],
+			cameraUpVector=[1, 0, 0]
+		)
 
-		for i in range(self.num_of_joints):
-			#print(p.getJointInfo(arm_id, i)[8:10])
-			limits.append(p.getJointInfo(self.arm_id, i)[8:10])
+		projection_matrix = p.computeProjectionMatrixFOV(
+			fov=60,
+			aspect=1.0,
+			nearVal=0.01,
+			farVal=100
+		)
 
-		return limits
+		img = p.getCameraImage(
+			width=224,
+			height=224,
+			viewMatrix=view_matrix,
+			projectionMatrix=projection_matrix
+		)
+
+		return img
 
 	def render(self, mode='human'):
 		if mode == 'human':
@@ -100,19 +113,31 @@ class PandaGripperEnv(gym.Env):
 	def step(self, action):
 		action = np.clip(action, -1, 1).astype(np.float32)
 
-		for i in range(self.num_of_joints):
-			action[i] = self._linear_map(action[i], -1, 1, self.limits[i][0], self.limits[i][1])
+		action = self._map_linear(action)
 
 		rew, done = self._compute_reward(action)
 
 		# execute action
-		p.setJointMotorControlArray(self.arm_id, list(range(self.num_of_joints)), controlMode=p.POSITION_CONTROL, targetPositions=action)
-		# time.sleep(1)
+		rel_action = (action.T - self.joint_states).T
 
-		if self.first_person_view:
-			pass
-		else:
-			img = p.getCameraImage(100, 100, self.view_matrix, self.projection_matrix)
+		# Compute max time needed to complete motion
+		TIME = np.max(rel_action.T / self.velocity_limits)
+		TIME = math.ceil(TIME / (1/240))
+
+		sub_action = rel_action / TIME
+		joint_ind = list(range(self.num_arm_joints))
+
+		for step in range(TIME):
+			target_pos = np.squeeze(self.joint_states + sub_action.T * step)
+			p.setJointMotorControlArray(self.arm_id, joint_ind, p.POSITION_CONTROL, targetPositions=target_pos)
+			p.stepSimulation()
+			time.sleep(1/240)
+
+		# Update joint states
+		for j in range(self.num_arm_joints):
+			self.joint_states[j] = p.getJointState(self.arm_id, j)[0]
+
+		img = self._get_camera_img()
 
 		return img, rew, done
 
@@ -137,7 +162,7 @@ class PandaGripperEnv(gym.Env):
 			done = 1
 
 		# movement penalty
-		current_joint_pos = np.array(p.getJointStates(self.arm_id, list(range(self.num_of_joints))))[:, 0]
+		current_joint_pos = np.array(p.getJointStates(self.arm_id, list(range(self.num_arm_joints))))[:, 0]
 
 		# penalty = sum(abs(current - target)) * some contstant
 		movement_penalty = np.absolute(action - current_joint_pos).sum(axis=0) * self.movement_penalty_constant
@@ -147,9 +172,13 @@ class PandaGripperEnv(gym.Env):
 		return reward, done
 
 
-	def _linear_map(self, x, in_min, in_max, out_min, out_max):
-		return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
-
+	def _map_linear(self, action):
+		for i in range(self.num_arm_joints):
+			val = action[i]
+			minimum = self.joint_limits[i][0]
+			maximum = self.joint_limits[i][1]
+			action[i] = (((val - (-1)) * (maximum - minimum)) / (1 - (-1))) + minimum
+		return action
 
 env = PandaGripperEnv(render=True)
 
