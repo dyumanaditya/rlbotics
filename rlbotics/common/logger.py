70c0046de13fbe57b86cc6e861406d704a24c50a
DyumanAditya <dyuman2001@gmail.com>
2020-10-27 16:28:32 +0530

    Fixed Logger and Plotter to read and write csv files of logs only once an epoch. Iteration parameter needs to be passed with any policy or return logging to avoid confusion

--- rlbotics/common/logger.py
+++ rlbotics/common/logger.py
@@ -6,7 +6,6 @@ import shutil
 import numpy as np
 import pandas as pd
 from torch.utils.tensorboard import SummaryWriter
-from rlbotics.common.utils import get_latest_ep_return
 
 
 class Logger:
@@ -29,43 +28,28 @@ class Logger:
 			os.makedirs(self.model_dir)
 
 		self.resume = resume
-		self.transition_keys, self.policy_update_keys = [], []
+		self.returns_header, self.policy_updates_header = True, True
 
 		# Tensor Board
 		self.writer = SummaryWriter(log_dir=self.log_dir)
-		if algo_name.lower() in ['dqn', 'ddqn', 'ddpg', 'td3']:
-			self.mean = False
-		elif algo_name.lower() in ['vpg', 'trpo', 'ppo', 'sac']:
-			self.mean = True
-
-		# Counter to track number of policy updates
-		self.tensorboard_updated = 0
-
-		# Keeps track of returns from episodes for each epoch
-		self.episode_returns = []
 
 		if self.resume:
 			self.resume_log()
 
-	def log(self, name='params', **kwargs):
-		if name == 'transitions':
-			file = os.path.join(self.log_dir, 'transitions.csv')
-			header = True if len(self.transition_keys) == 0 and not self.resume else False
-			if header:
-				self.transition_keys = list(kwargs.keys())
-			self._save_tabular(file, header, **kwargs)
+	def log_return(self, iteration, avg_return):
+		file = os.path.join(self.log_dir, 'returns.csv')
+		self._save_tabular(file, self.returns_header, iteration=iteration, avg_return=avg_return)
+		self.returns_header = False
 
-		elif name == 'policy_updates':
-			file = os.path.join(self.log_dir, 'policy_updates.csv')
-			header = True if len(self.policy_update_keys) == 0 and not self.resume else False
-			if header:
-				self.policy_update_keys = list(kwargs.keys())
-			self._save_tabular(file, header, **kwargs)
+	def log_policy_update(self, iteration, **kwargs):
+		file = os.path.join(self.log_dir, 'policy_updates.csv')
+		self._save_tabular(file, self.policy_updates_header, iteration=iteration, **kwargs)
+		self.policy_updates_header = False
 
-		elif name == 'params':
-			file = os.path.join(self.log_dir, 'params.json')
-			with open(file, 'w') as f:
-				json.dump(kwargs, f, indent=4)
+	def log_params(self, **kwargs):
+		file = os.path.join(self.log_dir, 'params.json')
+		with open(file, 'w') as f:
+			json.dump(kwargs, f, indent=4)
 
 	def _save_tabular(self, file, header, **kwargs):
 		with open(file, 'a') as f:
@@ -77,23 +61,13 @@ class Logger:
 		self._write_tensorboard(file, **kwargs)
 
 	def _write_tensorboard(self, file, **kwargs):
-		if 'transitions.csv' in file:
-			if kwargs.get('done'):
-				latest_return = get_latest_ep_return(file)
-				self.episode_returns.append(latest_return)
+		if 'returns.csv' in file:
+			self.writer.add_scalar('mean reward/timestep', kwargs['avg_return'], kwargs['iteration'])
 		else:
+			iteration = kwargs['iteration']
+			kwargs.pop('iteration')
 			for key, value in kwargs.items():
-				self.writer.add_scalar(key, value, self.tensorboard_updated)
-
-			if self.episode_returns:
-				if self.mean:
-					self.writer.add_scalar('mean reward/epoch', np.mean(self.episode_returns), self.tensorboard_updated)
-					self.tensorboard_updated += 1
-				else:
-					for rew in self.episode_returns:
-						self.writer.add_scalar('reward/epoch', rew, self.tensorboard_updated)
-						self.tensorboard_updated += 1
-				self.episode_returns.clear()
+				self.writer.add_scalar(key, value, iteration)
 
 	def log_model(self, mlp, name=''):
 		file = os.path.join(self.model_dir, name + 'model.pth')
@@ -104,11 +78,8 @@ class Logger:
 		torch.save(dct, file)
 
 	def resume_log(self):
-		log_file = os.path.join(self.log_dir, 'transitions.csv')
-		log = pd.read_csv(log_file)
-		for i in range(len(log)):
-			if log.loc[i, 'done']:
-				self.tensorboard_updated += 1
+		raise NotImplementedError
+
 
 class Loggerv2:
 	def __init__(self, algo_name, env_name, seed, resume=False):
--- rlbotics/common/plotter.py
+++ rlbotics/common/plotter.py
@@ -2,7 +2,6 @@ import os
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from rlbotics.common.utils import get_ep_returns
 
 
 class Plotter:
@@ -13,13 +12,15 @@ class Plotter:
 		if not os.path.exists(self.plt_dir):
 			os.makedirs(self.plt_dir)
 
-	def plot_individual(self, title, xlabel, ylabel, algo, env, seed, epoch_iter=1, display=False):
-		log_file = os.path.join(self.cur_dir, 'experiments', 'logs', f'{algo}_{env}_{seed}', 'transitions.csv')
-		ep_returns = get_ep_returns(log_file, epoch_iter)
+	def plot_individual(self, title, xlabel, ylabel, algo, env, seed, display=False):
+		log_file = os.path.join(self.cur_dir, 'experiments', 'logs', f'{algo}_{env}_{seed}', 'returns.csv')
+		log = pd.read_csv(log_file)
+		x = list(log['iteration'])
+		ep_returns = list(log['avg_return'])
 
 		# Plot
-		ep_returns = pd.Series(ep_returns).rolling(10, min_periods=1).mean()
-		ax = sns.lineplot(x=list(range(len(ep_returns))), y=ep_returns)
+		# ep_returns = pd.Series(ep_returns).rolling(10, min_periods=1).mean()
+		ax = sns.lineplot(x=x, y=ep_returns)
 		ax.axes.set_title(title, fontsize=20)
 		ax.set_xlabel(xlabel, fontsize=15)
 		ax.set_ylabel(ylabel, fontsize=15)
@@ -30,40 +31,39 @@ class Plotter:
 		if display:
 			plt.show()
 
-	def combine_csv_files(self, algo, env, epoch_iter, data='rewards', log_file_type='transitions'):
+	def combine_csv_files(self, algo, env, log_file_type):
 		# Combine all csv files with different seeds into x and y so we can plot
 		x = []
 		y = []
 		num_seeds = 10
 
-		if data == 'rewards':
+		if log_file_type == 'returns':
 			for seed in range(num_seeds):
-				filename = os.path.join('experiments', 'logs', f'{algo}_{env}_{seed}', f'{log_file_type}.csv')
-				returns = get_ep_returns(filename, epoch_iter)
-				x += list(range(len(returns)))
+				log_file = os.path.join('experiments', 'logs', f'{algo}_{env}_{seed}', f'{log_file_type}.csv')
+				log = pd.read_csv(log_file)
+				returns = list(log['avg_return'])
+				x += list(log['iteration'])
 				y += returns
-		else:
+		elif log_file_type == 'loss':
 			for seed in range(num_seeds):
 				filename = os.path.join('experiments', 'logs', f'{algo}_{env}_{seed}', f'{log_file_type}.csv')
 				df = pd.read_csv(filename)
-				col = list(df[data])
-				x += list(range(len(col)))
+				col = list(df['loss'])
+				x += list(df['iteration'])
 				y += col
 
 		return x, y
 
-	def plot_combined(self, title, xlabel, ylabel, algo, env, epoch_iter=1, display=False, data='rewards', log_file_type='transitions'):
+	def plot_combined(self, title, xlabel, ylabel, algo, env, display=False, log_file_type='returns'):
 		"""
-		:param epoch_iter: number of iterations for 1 epoch. (hyperparameters:max_iterations)
-		:param data: Either rewards or anything else you want to plot
 		:param log_file_type: Either transitions or policy_updates
 		"""
 		filename = os.path.join(self.plt_dir, 'all_seeds', algo)
 		if not os.path.exists(filename):
 			os.makedirs(filename)
 
-		x, y = self.combine_csv_files(algo, env, epoch_iter, data, log_file_type)
-		y = pd.Series(y).rolling(5, min_periods=1).mean()
+		x, y = self.combine_csv_files(algo, env, log_file_type)
+		# y = pd.Series(y).rolling(5, min_periods=1).mean()
 
 		# Plot
 		ax = sns.lineplot(x=x, y=y, ci=95)
@@ -76,6 +76,7 @@ class Plotter:
 		# Display
 		if display:
 			plt.show()
+
 
 class Plotterv2:
 	def __init__(self):
@@ -139,5 +140,5 @@ class Plotterv2:
 		return epoch_returns
 
 
-# p = Plotterv2()
-# p.plot_combined('LunarLander VPG', 'Epochs', 'Mean Return', 'VPG', 'LunarLanderContinuous-v2', display=True)
+p = Plotter()
+p.plot_combined('CartPole-v1 DQN', 'Timesteps', 'Mean Return', 'DQN', 'CartPole-v1', display=True)
