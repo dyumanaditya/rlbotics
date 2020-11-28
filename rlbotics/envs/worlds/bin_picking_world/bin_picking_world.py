a2b4b1d9ba5567cb856646bbfc47574520a48824
kousheekc <kousheekc@gmail.com>
2020-11-28 14:57:18 +0530

    folder structure for models

---
+++ rlbotics/envs/worlds/bin_picking_world/bin_picking_world.py
@@ -0,0 +1,14 @@
+
+
+class BinPickingWorld:
+    def __init__(self, robot, render):
+        pass
+
+    def reset(self):
+        pass
+
+    def add_random_object(self):
+        pass
+
+    def get_camera_img(self):
+        pass
