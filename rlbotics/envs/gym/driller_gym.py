51b125026d185507152c30c93cd1bd21c4ca24cf
DyumanAditya <dyuman2001@gmail.com>
2020-12-03 16:17:15 +0530

    Completed drilling gym and world

--- rlbotics/envs/gym/driller_gym.py
+++ rlbotics/envs/gym/driller_gym.py
@@ -1,9 +1,7 @@
 import gym
-import math
 import numpy as np
 import pybullet as p
 from gym import spaces
-from gym.utils import seeding
 
 from rlbotics.envs.common.domain_randomizer import DomainRandomizer
 from rlbotics.envs.worlds.driller_world import DrillerWorld
@@ -13,51 +11,47 @@ class DrillerGym(DrillerWorld, gym.Env):
 	metadata = {'render.modes': ['rgb', 'rgbd', 'rgbds'],
 				'video.frames_per_second': 50}
 
-	def __init__(self, robot, render=False, obs_mode='rgb', domain_randomization=True):
-		super().__init__(robot, render)
+	def __init__(self, robot, gripper, render=False, obs_mode='rgb', domain_randomization=True, use_ee_cam=False, rew_type='dense'):
+		super().__init__(robot, gripper, render, use_ee_cam)
 		self.domain_randomization = domain_randomization
 		self.obs_mode = obs_mode
+		self.rew_type = rew_type
 		self.max_timesteps = 1000
 		self.timestep = 0
 		self.done = False
 
 		# Initialise environment spaces
-		self.action_space = spaces.Box(-1, 1, (self.arm.num_joints,), dtype=np.float32)
-		if self.obs_mode == 'rgb':
-			self.observation_space = spaces.Box(0, 255, shape=(2, 224, 224, 3), dtype=np.uint8)
-		elif self.obs_mode == 'rgbd':
-			self.observation_space = spaces.Box(0.01, 1000, shape=(2, 224, 224, 4), dtype=np.uint16)
-		elif self.obs_mode == 'rgbds':
-			self.observation_space = spaces.Box(0.01, 1000, shape=(2, 224, 224, 5), dtype=np.uint16)
+		self.action_space = spaces.Box(-1, 1, (self.arm.arm_num_dof,), dtype=np.float32)
+		if use_ee_cam:
+			if self.obs_mode == 'rgb':
+				self.observation_space = spaces.Box(0, 255, shape=(224, 224, 3), dtype=np.uint8)
+			elif self.obs_mode == 'rgbd':
+				self.observation_space = spaces.Box(0.01, 1000, shape=(224, 224, 4), dtype=np.uint16)
+			elif self.obs_mode == 'rgbds':
+				self.observation_space = spaces.Box(0.01, 1000, shape=(224, 224, 5), dtype=np.uint16)
+		else:
+			if self.obs_mode == 'rgb':
+				self.observation_space = spaces.Box(0, 255, shape=(2, 224, 224, 3), dtype=np.uint8)
+			elif self.obs_mode == 'rgbd':
+				self.observation_space = spaces.Box(0.01, 1000, shape=(2, 224, 224, 4), dtype=np.uint16)
+			elif self.obs_mode == 'rgbds':
+				self.observation_space = spaces.Box(0.01, 1000, shape=(2, 224, 224, 5), dtype=np.uint16)
 
 		# Initialise env
-		self.seed()
 		self.domain_randomizer = DomainRandomizer(self.np_random)
 		self.reset()
-
-	def seed(self, seed=None):
-		self.np_random, seed = seeding.np_random(seed)
-		return [seed]
 
 	def reset(self):
 		self.done = False
 		self.timestep = 0
 		p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
 
-		# TODO: Reset drill position
-
-		self.arm.reset()
-		p.removeBody(self.hole)
-		p.removeBody(self.plane)
-
 		# Randomize physics constraints and drill color
 		self.domain_randomizer.randomize_physics_constraints(self.drill_id)
 		self.domain_randomizer.randomize_color(self.drill_id)
 
-		self.grab_drill()
-		self.generate_plane()
-
-		obs = self.render(mode=self.obs_mode)
+		self.reset_world()
+		obs = self._get_obs()
 		return obs
 
 	def step(self, action):
@@ -65,151 +59,155 @@ class DrillerGym(DrillerWorld, gym.Env):
 		action = np.clip(action, -1, 1).astype(np.float32)
 
 		# Map to appropriate range according to joint joint_limits
-		joint_angles = self._map_linear(action)
+		joint_angles = self._map_action_to_joint_space(action)
+		self.arm.set_joint_positions(joint_angles)
 
-		# TODO: Complete motion using robot base class
+		delta_joint_angles = joint_angles - self.arm.get_joint_positions(mode='arm')
+		rew = self._get_rew(delta_joint_angles)
+
+		new_obs = self._get_obs()
+		return new_obs, rew, self.done, {}
 
 	def render(self, mode='rgb'):
 		img = self.get_camera_img()
-		img1, img2, dep1, dep2, seg1, seg2 = img[0][0], img[0][1], img[1][0], img[1][1], img[2][0], img[2][1]
+		if self.use_ee_cam:
+			rgb, dep, seg = img[0], img[1], img[2]
 
-		if mode == 'rgb':
-			return img1, img2
+			if mode == 'rgb':
+				return rgb
 
-		elif mode == 'rgbd':
-			return np.dstack((img1, dep1)), np.dstack((img2, dep2))
+			elif mode == 'rgbd':
+				return rgb, dep
 
-		elif mode == 'rgbds':
-			return np.dstack((img1, dep1, seg1)), np.dstack((img2, dep2, seg2))
+			elif mode == 'rgbds':
+				return rgb, dep, seg
+
+		else:
+			rgb1, rgb2, dep1, dep2, seg1, seg2 = img[0][0], img[0][1], img[1][0], img[1][1], img[2][0], img[2][1]
+
+			if mode == 'rgb':
+				return rgb1, rgb2
+
+			elif mode == 'rgbd':
+				return rgb1, rgb2, dep1, dep2
+
+			elif mode == 'rgbds':
+				return rgb1, rgb2, dep1, dep2, seg1, seg2
 
 	def close(self):
 		p.disconnect()
 
-	def get_camera_img(self):
-		view_matrix1 = p.computeViewMatrix(
-			cameraEyePosition=[0, 0, 2.5],
-			cameraTargetPosition=[0, 0, 0],
-			cameraUpVector=[1, 0, 0]
-		)
+	def _get_obs(self):
+		img = self.render(mode=self.obs_mode)
 
-		view_matrix2 = p.computeViewMatrix(
-			cameraEyePosition=[-0.2, 1.5, 1.3],
-			cameraTargetPosition=[-0.2, 0, 1.4],
-			cameraUpVector=[0, 1, 0]
-		)
+		if self.use_ee_cam:
+			if self.obs_mode == 'rgb':
+				rgb = img
+				rgb = self.domain_randomizer.randomize_lighting(rgb) if self.domain_randomization else rgb
+				img = rgb
 
-		projection_matrix1 = p.computeProjectionMatrixFOV(
-			fov=30,
-			aspect=1.0,
-			nearVal=0.01,
-			farVal=2
-		)
+			elif self.obs_mode == 'rgbd':
+				rgb, dep = img
+				rgb = self.domain_randomizer.randomize_lighting(rgb) if self.domain_randomization else rgb
+				img = np.dstack((rgb, dep))
 
-		projection_matrix2 = p.computeProjectionMatrixFOV(
-			fov=40,
-			aspect=1.0,
-			nearVal=0.01,
-			farVal=2
-		)
+			elif self.obs_mode == 'rgbds':
+				rgb, dep, seg = img
+				rgb = self.domain_randomizer.randomize_lighting(rgb) if self.domain_randomization else rgb
+				img = np.dstack((rgb, dep, seg))
 
-		_, _, rgb_img1, depth_img1, seg_img1 = p.getCameraImage(
-			width=224,
-			height=224,
-			viewMatrix=view_matrix1,
-			projectionMatrix=projection_matrix1
-		)
+		else:
+			if self.obs_mode == 'rgb':
+				rgb1, rgb2 = img
+				rgb1, rgb2 = self.domain_randomizer.randomize_lighting(rgb1, rgb2) if self.domain_randomization else rgb1, rgb2
+				img = rgb1, rgb2
 
-		_, _, rgb_img2, depth_img2, seg_img2 = p.getCameraImage(
-			width=224,
-			height=224,
-			viewMatrix=view_matrix2,
-			projectionMatrix=projection_matrix2
-		)
+			elif self.obs_mode == 'rgbd':
+				rgb1, rgb2, dep1, dep2 = img
+				rgb1, rgb2 = self.domain_randomizer.randomize_lighting(rgb1, rgb2) if self.domain_randomization else rgb1, rgb2
+				img = np.dstack((rgb1, dep1)), np.dstack((rgb2, dep2))
 
-		# Remove alpha channel
-		rgb_img1, rgb_img2 = rgb_img1[:, :, :3], rgb_img2[:, :, :3]
-		if self.domain_randomization:
-			rgb_img1, rgb_img2 = self.domain_randomizer.randomize_lighting(rgb_img1, rgb_img2)
+			elif self.obs_mode == 'rgbds':
+				rgb1, rgb2, dep1, dep2, seg1, seg2 = img
+				rgb1, rgb2 = self.domain_randomizer.randomize_lighting(rgb1, rgb2) if self.domain_randomization else rgb1, rgb2
+				img = np.dstack((rgb1, dep1, seg1)), np.dstack((rgb2, dep2, seg2))
 
-		return [(rgb_img1, rgb_img2), (depth_img1, depth_img2), (seg_img1, seg_img2)]
+		return img
 
-	def _map_linear(self, joint_angles):
+	def _map_action_to_joint_space(self, action):
+		joint_positions = np.zeros_like(action)
 		for i in range(self.arm.num_joints):
-			val = joint_angles[i]
+			val = action[i]
 			minimum = self.arm.joint_lower_limits[0]
 			maximum = self.arm.joint_upper_limits[1]
-			joint_angles[i] = (((val - (-1)) * (maximum - minimum)) / (1 - (-1))) + minimum
-		return joint_angles
+			joint_positions[i] = (((val - (-1)) * (maximum - minimum)) / (1 - (-1))) + minimum
+		return joint_positions
 
-	def _compute_reward(self, delta_joint_angles, sparse=False):
-		# TODO: FIX THIS!!
-		reward = 0
+	def _get_rew(self, delta_joint_angles):
+		rew = 0
 
-		if sparse:
+		if self.rew_type == 'dense':
 			# Check if drill has dropped
-			c1 = len(p.getContactPoints(self.arm_id, self.drill_id, linkIndexA=9))
-			c2 = len(p.getContactPoints(self.arm_id, self.drill_id, linkIndexA=10))
+			c1 = len(p.getContactPoints(self.arm.robot_id, self.drill_id, linkIndexA=self.arm.gripper_joint_indices[0],
+										physicsClientId=self.physics_client))
+			c2 = len(p.getContactPoints(self.arm.robot_id, self.drill_id, linkIndexA=self.arm.gripper_joint_indices[1],
+										physicsClientId=self.physics_client))
 			if c1 + c2 == 0:
 				self.done = True
-				reward = -1
+				rew -= 300
 
-			# Check if drilling task is complete
-			self._update_drill_vector()
-			cos_theta = np.dot(self.drill_bit_vector, self.plane_normal)/(np.linalg.norm(self.drill_bit_vector)*np.linalg.norm(self.plane_normal))
-			theta = math.degrees(math.acos(cos_theta))
-
-			drill_pos = np.array(p.getBasePositionAndOrientation(self.drill_id)[0])
-			targ_dist = drill_pos - self.hole_pos
-			if theta < 1 and np.linalg.norm(targ_dist) <= 0.101:
-				reward = 1
-				self.done = True
-		else:
-			# Check if drill has dropped
-			c1 = len(p.getContactPoints(self.arm_id, self.drill_id, linkIndexA=9))
-			c2 = len(p.getContactPoints(self.arm_id, self.drill_id, linkIndexA=10))
-			if c1 + c2 == 0:
-				self.done = True
-				reward -= 200
 			# Check if drill is touching the table
-			elif len(p.getContactPoints(self.drill_id, self.table_id)) != 0 and not self.done:
-				reward -= 200
-			# Check if Panda is touching the table
-			if len(p.getContactPoints(self.arm_id, self.table_id)) != 0:
-				reward -= 200
-			# Check if Panda is touching the plane
-			if len(p.getContactPoints(self.arm_id, self.plane)) != 0:
-				reward -= 200
+			if len(p.getContactPoints(self.drill_id, self.table_id, physicsClientId=self.physics_client)) != 0 and not self.done:
+				rew -= 200
 
-			# Check angle between drill bit and plane normal
-			angle_scale = 2
-			self._update_drill_vector()
-			cos_theta = np.dot(self.drill_bit_vector, self.plane_normal)/(np.linalg.norm(self.drill_bit_vector)*np.linalg.norm(self.plane_normal))
-			theta = math.degrees(math.acos(cos_theta))
-			reward -= theta * angle_scale
-
-			# Check if drilling task is complete
-			dist_scale = 10
-			drill_pos = np.array(p.getBasePositionAndOrientation(self.drill_id)[0])
-			targ_dist = np.linalg.norm(drill_pos - self.hole_pos)
-			reward -= targ_dist * dist_scale
-			if theta < 1 and targ_dist <= 0.102:
-				reward += 500
-				self.done = True
+			# Check if robot is touching the drilling plane
+			if len(p.getContactPoints(self.arm.robot_id, self.plane, physicsClientId=self.physics_client)) != 0:
+				rew -= 200
 
 			# Compute electricity cost
-			electricity_scale = 1
-			electricity_cost = np.sum(np.abs(delta_joint_angles) * electricity_scale)
+			electricity_scale = 2
+			rew -= np.sum(np.abs(delta_joint_angles) * electricity_scale)
 
-			# Compute final reward
-			reward = reward - electricity_cost
+			if not self.done:
+				# Get distance and angle between target hole and drillbit
+				distance, angle = self.get_drillbit_hole_distance_and_angle()
 
-			if self.timestep == self.max_timesteps:
-				reward = -1 if sparse and not self.done else reward
-				self.timestep = 0
+				# Check if task is performed
+				if angle <=0.01 and distance <= 0.001:
+					self.done = True
+					rew += 500
+				distance_scale = 100
+				angle_scale = 1
+				distance *= distance_scale
+				angle *= angle_scale
+
+				rew += 100 - distance
+				rew += 100 - angle
+
+		else:
+			# Sparse rewards
+			# Check if drill has dropped
+			c1 = len(p.getContactPoints(self.arm.robot_id, self.drill_id, linkIndexA=self.arm.gripper_joint_indices[0],
+										physicsClientId=self.physics_client))
+			c2 = len(p.getContactPoints(self.arm.robot_id, self.drill_id, linkIndexA=self.arm.gripper_joint_indices[1],
+										physicsClientId=self.physics_client))
+			if c1 + c2 == 0:
 				self.done = True
+				rew = -1
 
-		return reward
+			if not self.done:
+				# Get distance and angle between target hole and drillbit
+				distance, angle = self.get_drillbit_hole_distance_and_angle()
 
+				# Check if task is performed
+				if angle <=0.01 and distance <= 0.001:
+					self.done = True
+					rew = 1
 
+		if self.timestep == self.max_timesteps:
+			timestep_penalty = 100
+			rew = -1 if self.rew_type == 'sparse' and not self.done else rew - timestep_penalty
+			self.timestep = 0
+			self.done = True
 
-
+		return rew
