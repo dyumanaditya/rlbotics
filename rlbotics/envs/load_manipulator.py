1641ebd678be5e79dfd7eedc975b84c456b3f737
kousheekc <kousheekc@gmail.com>
2021-01-06 15:15:46 +0530

    body in white world

--- rlbotics/envs/load_manipulator.py
+++ rlbotics/envs/load_manipulator.py
@@ -1,6 +1,6 @@
 from rlbotics.envs.robots.universal_robots import UR10, UR5, UR3
 from rlbotics.envs.robots.panda import Panda
-from rlbotics.envs.robots.kuka import Iiwa
+from rlbotics.envs.robots.kuka import KukaIiwa
 
 import pybullet as p
 import numpy as np
@@ -11,7 +11,7 @@ p.setGravity(0, 0, -9.8)
 
 robot1 = UR10(physics_client, [0,0,0], [0,0,0,1], gripper_name='robotiq_2f_85')
 # robot2 = UR5(physics_client, [0,1,0], [0,0,0,1], gripper_name='robotiq_2f_85')
-# # robot3 = Iiwa(physics_client, [0,0.5,0], [0,0,0,1], gripper_name='robotiq_2f_85')
+# robot3 = Iiwa(physics_client, [0,0.5,0], [0,0,0,1], gripper_name='robotiq_2f_85')
 # robot4 = Panda(physics_client, [0,0,0], [0,0,0,1])
 
 robot1.reset()
---
+++ rlbotics/envs/worlds/body_in_white_world.py
@@ -0,0 +1,73 @@
+from rlbotics.envs.robots.universal_robots import UR10, UR5, UR3
+from rlbotics.envs.robots.panda import Panda
+from rlbotics.envs.robots.kuka import KukaIiwa
+
+import pybullet as p
+import pybullet_data
+import numpy as np
+import time
+import os
+
+class BodyInWhiteWorld:
+    def __init__(self, robot, gripper, render, use_ee_cam=False):
+        self.use_ee_cam = use_ee_cam
+        self.gripper = gripper
+        self.physics_client = p.connect(p.GUI) if render else p.connect(p.DIRECT)
+        self.path = os.path.abspath(os.path.dirname(__file__))
+        p.setGravity(0, 0, -9.8)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+        self.plane_id = p.loadURDF('plane.urdf', physicsClientId=self.physics_client)
+
+
+
+    def reset_world(self):
+        object_orientation = p.getQuaternionFromEuler([0,0,np.pi/2])
+
+        object_visual = p.createVisualShape(
+            p.GEOM_MESH,
+            meshScale=[0.001] * 3,
+            fileName=os.path.join(os.path.dirname(self.path), 'models', 'misc', 'body_in_white', 'car1.obj'),
+            rgbaColor=[0.5, 0.5, 0.5]
+        )
+
+        object_collision = p.createCollisionShape(
+            p.GEOM_MESH,
+            meshScale=[0.001] * 3,
+            fileName=os.path.join(os.path.dirname(self.path), 'models', 'misc', 'body_in_white', 'car1.obj'),
+        )
+
+        self.body_in_white = p.createMultiBody(
+            basePosition=[0,0,0],
+            baseVisualShapeIndex=object_visual,
+            baseCollisionShapeIndex=object_collision,
+            baseOrientation=object_orientation,
+            baseMass=1000,
+            baseInertialFramePosition=[1, 0, 0]
+        )
+
+        print("finished loading car")
+
+        self.robot1 = Panda(self.physics_client, [-1.5,-0,0], [0,0,0,1], gripper_name=self.gripper)
+        self.robot2 = Panda(self.physics_client, [1.5,-0,0], [0,0,1,0], gripper_name=self.gripper)
+        # self.robot3 = Panda(self.physics_client, [-1.5,1.5,0], [0,0,0,1], gripper_name=self.gripper)
+        # self.robot4 = Panda(self.physics_client, [1.5,1.5,0], [0,0,1,0], gripper_name=self.gripper)
+        self.robot5 = Panda(self.physics_client, [-1.5,3,0], [0,0,0,1], gripper_name=self.gripper)
+        self.robot6 = Panda(self.physics_client, [1.5,3,0], [0,0,1,0], gripper_name=self.gripper)
+
+        self.robot1.reset()
+        self.robot2.reset()
+        # self.robot3.reset()
+        # self.robot4.reset()
+        self.robot5.reset()
+        self.robot6.reset()
+
+
+
+world = BodyInWhiteWorld('UR10', 'robotiq_2f_85', render=True)
+
+world.reset_world()
+
+while True:
+    time.sleep(1)
+
